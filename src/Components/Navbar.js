2e4ba728f63c0b8a9dc8d21e51bca5fc4171fdb1
LeviBlack <levi.t.black.lb@gmail.com>
2021-09-23 12:26:41 -0700

    implementing react hooks "useContext" instead of context api consumers and providers

--- src/Components/Navbar.js
+++ src/Components/Navbar.js
@@ -1,39 +1,32 @@
-import React, {Component} from 'react'
+import React, {Component, useContext} from 'react'
 import { ThemeContext } from '../Context/ThemeContext'
 import { Card, CardContent } from '@material-ui/core'
 import { AuthContext } from '../Context/AuthContext'
 
 
-
-
-class Navbar extends Component {
-    render() {
-        return (
-            <AuthContext.Consumer>{(authContext) => (
-                <ThemeContext.Consumer>{(themeContext) => {
-                    const {isAuthenticated, toggleAuth} = authContext
-                    const { isLightTheme, light, dark } = themeContext
-                    const theme = isLightTheme ? light : dark
-                        return (
-                        <Card>
-                            <nav style={{ background: theme.ui, color: theme.syntax}}>
-                                <h1>Context API App</h1>
-                                <div onClick={toggleAuth}>
-                                {isAuthenticated ? "Logged in" : "Logged out"}
-                                </div>
-                                    <ul>
-                                        <li>home</li>
-                                        <li>about</li>
-                                        <li>contact</li>
-                                    </ul>
-                            </nav>
-                        </Card>
-                        )
-                }}</ThemeContext.Consumer>
-            )}</AuthContext.Consumer>
-        )
-    }
+const Navbar = () => {
+    const {isLightTheme, light, dark} = useContext(ThemeContext)
+    const theme = isLightTheme ? light : dark
+    const { isAuthenticated, toggleAuth } = useContext(AuthContext)
+    return (
+        <Card>
+            <nav style={{ background: theme.ui, color: theme.syntax}}>
+                <h1>Context API App</h1>
+                <div onClick={toggleAuth}>
+                {isAuthenticated ? "Logged in" : "Logged out"}
+                </div>
+                    <ul>
+                        <li>home</li>
+                        <li>about</li>
+                        <li>contact</li>
+                    </ul>
+            </nav>
+        </Card>
+    )
 }
+
+
+
 
 export default Navbar
 
